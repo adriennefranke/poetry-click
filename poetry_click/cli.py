cfeee489ede9101137a914f0eefea80c9cc4cd42
adriennefranke <18449966+adriennefranke@users.noreply.github.com>
2020-06-28 13:35:21 -0500

    Create CLI with Click to interface with the brewerydb API

--- poetry_click/cli.py
+++ poetry_click/cli.py
@@ -1,4 +1,22 @@
 import requests
 import click
+import json
+from pygments import highlight, lexers, formatters
 
-print("You're ready to make a CLI.")
+def format_json_response(json_data):
+    formatted_json = json.dumps(json_data, sort_keys=True, indent=4)
+    highlighted_json = highlight(formatted_json, lexers.JsonLexer(), formatters.TerminalFormatter())
+    return highlighted_json
+
+@click.command()
+@click.option('--name', help='name of the beer')
+def search_beers(name):
+    click.echo(click.style('searching brewerydb...🍻', fg='blue'))
+    brewerydb_url = 'https://sandbox-api.brewerydb.com/v2/search'
+    payload = {'key': '89464b3cc474b22ea5dee881ca0b6360', 'type': 'beer', 'q': name}
+    r = requests.get(brewerydb_url, params=payload)
+    json_data = r.json()
+    click.echo(format_json_response(json_data))
+
+if __name__ == '__main__':
+    search_beers()
